d620b0ca64a185d67878ad960e86c339c7e4f0dd
NF92 Etudiant <nf92a065@tuxa.sme.utc>
2024-11-08 18:27:00 +0100

    Small

--- ajout_seance.php
+++ ajout_seance.php
@@ -1,13 +1,13 @@
 <html>
 	<head>
 		<meta charset = "UTF-8">
-		<Title> Ajout d'un séance </title>
+		<Title> Ajout d'une séance </title>
 		<link type = "text/css" rel = "stylesheet" href = "style.css">
 	</head>
 	<body>
 		<h1>Ajouter un séance</h1>
 		<p>
-		Pour ajoute un séance, choisissez les composantes et cliquez sur ajouter.
+		Pour ajoute une séance, choisissez les composantes et cliquez sur ajouter.
 		</p>
 		<form action = "ajouter_seance.php" method = "POST">
 		
@@ -35,14 +35,6 @@
 				
 				}
 
-				$query = "select * from seances as s1 join seances as s2 where s1.DateSeance = s2.DateSeance and s1.idTheme = s2.idTheme";
-
-				$result = mysqli_query($connect, $query);
-				while ($row = mysqli_fetch_array($result, MYSQLI_NUM)){
-					if (!$row[2]){
-						printf("<br>Conflit : %s %s", $row[0], $row[3]);		
-					}
-				}
 				mysqli_close($connect)
 
 
@@ -52,7 +44,28 @@
 		<br>
 		
 		<input type = "Submit" value = "Ajouter">
-		
+
+
+		<?php
+				$dbhost = "tuxa.sme.utc";
+				$dbuser = "nf92a065";
+				$dbpass = "ghdLQ90Fv3fr";
+				$dbname = "nf92a065";
+
+				$connect = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die("Unable to reach database");
+
+				mysqli_set_charset($connect, 'utf8');
+				$query = "select * from seances as s1 join seances as s2 where s1.DateSeance = s2.DateSeance and s1.idTheme = s2.idTheme and s1.idSeance < s2.idSeance";
+
+				$result = mysqli_query($connect, $query);
+				while ($row = mysqli_fetch_array($result, MYSQLI_NUM)){
+					
+						printf("<br>Conflit : %s %s", $row[0], $row[4]);		
+					
+				
+				}
+				mysqli_close($connect);
+		?>	
 		
 	</body>
 </html>
