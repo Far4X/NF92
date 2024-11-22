fda5cf886fd08d8602278ad24aa8081ddbc7b3db
NF92 Etudiant <nf92a065@tuxa.sme.utc>
2024-11-22 10:53:44 +0100

    Fixes

--- desinscription_seance.php
+++ desinscription_seance.php
@@ -18,16 +18,16 @@
 
             $connect = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
-            $seance = $_POST("id_seance");
-            if (empty(seance)){
+        
+            if (empty($_POST["id_seance"])){
                 echo "Veuillez sélectionner une séance (seules les séances avec un ou plusieurs inscrit sont renseignées) : <hr>";
-                echo "<form method = 'POST' action = 'desinscription_eleve'>";
+                $query = "SELECT sc.idSeance, sc.DateSeance, th.Nom FROM seances AS sc JOIN themes AS th ON th.idTheme = sc.IdTheme WHERE sc.DateSeance >= '".date("Y\-m\-d")."' AND 
+            (SELECT COUNT(*) FROM inscriptions AS ins WHERE ins.idseance = sc.idSeance) > 0
+";
+                echo $query;
+                echo "<form method = 'POST' action = 'desinscription_eleve.php'>";
                 echo "<select size = '4' name = 'id_seance'>";
                  
-                $query = "SELECT sc.idSeance, sc.DateSeance, th.Nom FROM seances AS sc WHERE sc.DateSeance >= ".date("Y\-m\-d")." AND 
-            (SELECT COUNT(*) FROM inscriptions AS ins WHERE ins.idseance = sc.idSeance) > 0
-                JOIN themes AS th ON th.idTheme = sc.IdTheme";
-                echo $query;
                 
                 $result = mysqli_query($connect, $query);
                 while ($row = mysqli_fetch_array($result)){
@@ -35,7 +35,7 @@
                 }
                 echo "</select><br><input type = 'submit' value = 'Envoyer'></form>";
 
-            }
+	    }
 
             mysqli_close($connect);
             ?>
